b70adbb9d63203837bc1f7a9918c21712e5dbe05
Aditya Rawat <adityarawat240@gmail.com>
2025-06-27 01:18:18 +0530

    ui skeleton result page

--- components/ResultPage.tsx
+++ components/ResultPage.tsx
@@ -2,6 +2,7 @@ import { restartSameTestAtom, shadowTestAtom } from "@/app/store/atoms/restartSa
 import { useAtom, useSetAtom } from "jotai";
 import { ChevronRight, ListRestart, RotateCcw } from "lucide-react";
 import { Dispatch, SetStateAction } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export default function ResultPage({
   setShowResultPage,
@@ -14,18 +15,36 @@ export default function ResultPage({
 }) {
   const setRestartSameTest = useSetAtom(restartSameTestAtom)
   const shadowTest = useAtom(shadowTestAtom)
-  console.log({shadowTest})
-    console.log(charArray)
     function handleClick(state:boolean) {
       setRestartSameTest(state)
       setShowResultPage(false)
     }
-  return <div className="w-screen h-screen flex flex-col items-center justify-center bg-red-300">
-    500 wpm = world record ohh maah godd
-
-    <div className="flex gap-4">
-      <RotateCcw onClick={()=>handleClick(true)}/>
-      <ChevronRight onClick={()=>handleClick(false)}/>
+  return <div className="w-screen h-screen flex flex-col items-center justify-start pt-12 gap-3 bg-red-300">
+    <div className="wpms w-full flex h-20 bg-green-500 items-center text-md sm:text-2xl justify-around">
+      <p>Raw wpm</p>
+      <p>wpm</p>
+      <p>Accuracy</p>
+    </div>
+    <div className="graph h-96 bg-red-400 w-full"></div>
+    <div className="flex items-center justify-center gap-10 h-20 w-full bg-blue-500">
+      
+      <Tooltip>
+        <TooltipTrigger>
+          <RotateCcw onClick={()=>handleClick(true)} className="cursor-pointer"/>
+        </TooltipTrigger>
+        <TooltipContent side="bottom">
+          <p className="p-2">Repeat Test</p>
+        </TooltipContent>
+      </Tooltip>
+      <Tooltip>
+        <TooltipTrigger>
+          <ChevronRight onClick={()=>handleClick(false)}  className="cursor-pointer"/>
+        </TooltipTrigger>
+        <TooltipContent side="bottom">
+          <p className="p-2">Next Test</p>
+        </TooltipContent>
+      </Tooltip>
+      
     </div>
     </div>;
 }
