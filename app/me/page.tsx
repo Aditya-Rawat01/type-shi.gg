ed39d93e9cd7fb2e601c85fcde4e5112672d7aa8
ADITYA RAWAT <adityarawat240@gmail.com>
2025-07-21 00:29:27 +0530

    cant run npm run build locally due to some issue, so here goes nothing

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -2,15 +2,31 @@ import ProfilePage from "@/components/profilePage";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { cookieType } from "../store/atoms/userCookie";
 
 export default async function page() {
       const sessionCookie = await auth.api.getSession({ headers: await headers() })
       if (!sessionCookie) {
         redirect("/login")
     }
+    const typeCorrectedUser = {...sessionCookie,
+        session:{
+            ...sessionCookie.session,
+            expiresAt: sessionCookie.session.expiresAt.toISOString(),
+            createdAt: sessionCookie.session.createdAt.toISOString(),
+            updatedAt: sessionCookie.session.updatedAt.toISOString()
+
+        },
+        user: {
+            ...sessionCookie.user,
+            createdAt: sessionCookie.user.createdAt.toISOString(),
+            updatedAt: sessionCookie.user.updatedAt.toISOString()
+        }
+    }
+    
     return (
         <div className="h-screen w-screen relative">
-            <ProfilePage sessionCookie={sessionCookie}/>
+            <ProfilePage sessionCookie={typeCorrectedUser}/>
         </div>
     )
 } 
