ab83e025f38250163fd15ab7cec5e6418a4103b1
Aditya Rawat <132136880+Aditya-Rawat01@users.noreply.github.com>
2026-02-05 15:18:32 +0530

    Update URI to new endpoint

--- lib/URI.ts
+++ lib/URI.ts
@@ -1,4 +1,4 @@
-export const URI = 'https://type-shi-gg.vercel.app' 
+export const URI = 'https://type-shi.ardev.in' 
 // check for better auth url at the .env file as well.
 // http://localhost:3000
 // change the auth callback at the google cloud console and github so the oAuth works correctly.
